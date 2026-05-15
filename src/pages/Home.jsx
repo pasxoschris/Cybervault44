9edac24ed08d10244aa84c4c91672c3a8ba5813d
Chris Paschos <pasxoschrisw@gmail.com>
2026-05-15 11:39:38 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import Footer from '@/components/layout/Footer';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-[#0A0E1A] text-white overflow-x-hidden">
+    <div className="min-h-screen text-white overflow-x-hidden" style={{ backgroundColor: '#0E1235', backgroundImage: 'url(https://media.base44.com/images/public/6a06d65e120e7e74497bab7a/e69e1c2c7_generated_image.png)', backgroundSize: 'cover', backgroundPosition: 'center', backgroundAttachment: 'fixed' }}>
       <Navbar />
       <HeroSection />
       <ServicesSection />
