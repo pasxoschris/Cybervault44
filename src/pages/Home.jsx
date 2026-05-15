ccbca16307a4c33e85c1d8844cad769cce2f150f
Chris Paschos <pasxoschrisw@gmail.com>
2026-05-15 11:49:14 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import Footer from '@/components/layout/Footer';
 
 export default function Home() {
   return (
-    <div className="min-h-screen text-white overflow-x-hidden" style={{ backgroundColor: '#0E1235', backgroundImage: 'url(https://media.base44.com/images/public/6a06d65e120e7e74497bab7a/e69e1c2c7_generated_image.png)', backgroundSize: 'cover', backgroundPosition: 'center', backgroundAttachment: 'fixed' }}>
+    <div className="min-h-screen text-white overflow-x-hidden" style={{ backgroundColor: '#0E1235', backgroundImage: 'url(https://media.base44.com/images/public/6a06d65e120e7e74497bab7a/51f2bb364_generated_image.png)', backgroundSize: 'cover', backgroundPosition: 'center', backgroundAttachment: 'fixed' }}>
       <Navbar />
       <HeroSection />
       <ServicesSection />
